6e5bb9bd40ee20252a7bea14f5cbe16ca78bae26
Nikita Konev <nikit.cpp@yandex.ru>
2023-07-08 21:01:39 +0400

    getters and setters, constructor

--- src/main/java/com/example/neo4jgraphql/model/Account.java
+++ src/main/java/com/example/neo4jgraphql/model/Account.java
@@ -11,12 +11,57 @@ import java.util.List;
 public class Account {
 
     @Id
-    String id;
-    String username;
-    @Property("display_name") String displayName;
-    @Relationship("REGISTERED_ON") Server server;
+    private String id;
+    private String username;
+    @Property("display_name")
+    private String displayName;
+    @Relationship("REGISTERED_ON")
+    private Server server;
     @Relationship("FOLLOWS")
-    List<Account> following;
+    private List<Account> following;
+
+    public Account() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Server getServer() {
+        return server;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    public List<Account> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(List<Account> following) {
+        this.following = following;
+    }
 
     // constructor, etc.
 }
--- src/main/java/com/example/neo4jgraphql/model/Server.java
+++ src/main/java/com/example/neo4jgraphql/model/Server.java
@@ -11,11 +11,57 @@ import java.util.List;
 public class Server {
 
     @Id
-    String uri;
-    String title;
-    @Property("registrations") Boolean registrationsAllowed;
-    @Property("short_description") String shortDescription;
-    @Relationship("CONNECTED_TO") List<Server> connectedServers;
+    private String uri;
+    private String title;
+    @Property("registrations")
+    private Boolean registrationsAllowed;
+    @Property("short_description")
+    private String shortDescription;
+    @Relationship("CONNECTED_TO")
+    private List<Server> connectedServers;
+
+    public Server() {
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getRegistrationsAllowed() {
+        return registrationsAllowed;
+    }
+
+    public void setRegistrationsAllowed(Boolean registrationsAllowed) {
+        this.registrationsAllowed = registrationsAllowed;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public List<Server> getConnectedServers() {
+        return connectedServers;
+    }
+
+    public void setConnectedServers(List<Server> connectedServers) {
+        this.connectedServers = connectedServers;
+    }
 
     // constructor, etc.
 }
